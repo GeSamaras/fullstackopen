9cece16e19609cb9fde7b51c13c0fb77fae9e830
GeSamaras <georgiosamaras@hotmail.com>
2024-04-14 14:54:53 +0200

    all courseinfo exercises

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,7 +1,9 @@
-import Course from "./components/Course"
+import Course from './components/Course'
+
 
 const App = () => {
-  const course = {
+  const course = [ 
+    {
     id: 1,
     name: 'Half Stack application development',
     parts: [
@@ -10,7 +12,7 @@ const App = () => {
         exercises: 10,
         id: 1
       },
-    {
+      {
         name: 'Using props to pass data',
         exercises: 7,
         id: 2
@@ -19,11 +21,41 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  },
+
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
       }
     ]
   }
+]
 
-  return <Course course={course} />
+
+const courseNumber = course.map(courses => { return <Course key={courses.id} course={courses}/>})
+
+  return (
+    <div>
+       {courseNumber}
+    </div>
+  )
 }
 
 export default App
